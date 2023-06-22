de23eb8316102a026df8aedde843c9a7aa6db83c
Mohieddine-Houttis <mohieddinehouttis970@gmail.com>
2023-06-22 08:34:01 -0700

    Your commit message here

---
+++ 0x03-debugging/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Step 1: Git add .
+git add .
+# Step 2: Git commit -m 'comment'
+commit_message="Your commit message here"
+git commit -m "$commit_message"
+# Step 3: Git push 
+git push
