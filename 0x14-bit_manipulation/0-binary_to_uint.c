e8000b03de20fe8208f30a139f9ca37f9b98afff
Mohieddine-Houttis <mohieddinehouttis970@gmail.com>
2023-08-04 16:40:21 -0700

    task 1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-
-
 /**
- * binary_to_uint - converts a binary_number to unsigned int
- * @b: string containing the binary_number
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
  *
- * Return: the converted nmber
+ * Return: the converted number
  */
 
 
